90320d0cba842295f2d0ac85acef095b5a50f661
Thomas Gehrmann <s6thgehr@gmail.com>
2022-06-09 10:46:01 +0200

    Minor change

--- calculator.js
+++ calculator.js
@@ -21,8 +21,8 @@ app.get("/bmicalculator", function (req, res) {
 });
 
 app.post("/bmicalculator", function (req, res) {
-  let weight = Number(req.body.weight);
-  let height = Number(req.body.height);
+  let weight = parseFloat(req.body.weight);
+  let height = parseFloat(req.body.height);
   let bmi = (weight / (height * height)) * 10000;
 
   res.send("<h3>Your BMI is " + bmi);
